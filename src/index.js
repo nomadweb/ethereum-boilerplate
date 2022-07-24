fb4e3117afc0f4624ffdec5fbef0c49dab1cb7a7
Malcolm <Bild96@pm.me>
2022-07-24 20:53:35 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,3 +44,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
